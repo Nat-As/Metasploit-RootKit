fe2cc844bac443adb7569cfdd7a35716f1455bdc
James Andrews <42288915+Nat-As@users.noreply.github.com>
2018-09-28 11:25:58 -0400

    Root Access to Metasploit Modules

--- metasploit-modules.sh
+++ metasploit-modules.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
-printf "Rooting...\n"
-chmod -c -R a+rwx /usr/share/metasploit-framework/modules/exploits
-printf "Done!\n"
+# Metasploit Rootkit By: James Andrews
+# Github: Nat-As
+# Fri 28 Sep 2018 11:18:16 AM EDT 
+
+clear
+cat << "EOF"
+   *    (       (                     )          
+ (  `   )\ )    )\ )            )  ( /(       )  
+ )\))( (()/(   (()/(         ( /(  )\())(  ( /(  
+((_)()\ /(_))__ /(_))(    (  )\())((_)\ )\ )\()) 
+(_()((_|_))|___(_))  )\   )\(_))/|_ ((_|(_|_))/  
+|  \/  / __|   | _ \((_) ((_) |_ | |/ / (_) |_   
+| |\/| \__ \   |   / _ \/ _ \  _|  ' <  | |  _|  
+|_|  |_|___/   |_|_\___/\___/\__| _|\_\ |_|\__|  
+                                                 
+===============================================
+BY: James Andrews
+===============================================
+EOF
+printf " Type ROOT to begin...\n"
+read root
+
+if [[ $root = ROOT ]]
+    then
+    printf "Rooting...\n"
+    sleep 5
+    chmod -c -R a+rwx /usr/share/metasploit-framework/modules/exploits
+    printf "Done!\n"
+fi
+
